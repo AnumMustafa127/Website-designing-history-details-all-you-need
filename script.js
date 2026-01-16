371c3d98cc121406dbc45bb23598f0a751722100
AnumMustafa127 <anummustafa9924@gmail.com>
2026-01-16 23:49:07 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,138 @@
+// -------- HISTORY POPUP --------
+const historyPopup = document.getElementById('popup1');
+const historyBtn = document.getElementById('history');
+const closeHistory = document.getElementById('button1');
+
+historyBtn.onclick = () => {
+    historyPopup.classList.add('show');
+    mainContent.classList.add('hide');
+    navHead.classList.add('hide');
+};
+
+closeHistory.onclick = () => {
+    historyPopup.classList.remove('show');
+    mainContent.classList.remove('hide');
+    navHead.classList.remove('hide');
+};
+
+// -------- INTRO POPUP --------
+const introPopup = document.getElementById('popup2');
+const introBtn = document.getElementById('intro');
+const closeIntro = document.getElementById('button2');
+
+introBtn.onclick = () => {
+    introPopup.classList.add('show');
+    mainContent.classList.add('hide');
+    navHead.classList.add('hide');
+};
+
+closeIntro.onclick = () => {
+    introPopup.classList.remove('show');
+    mainContent.classList.remove('hide');
+    navHead.classList.remove('hide');
+};
+
+
+// -------- working POPUP --------
+const workingPopup = document.getElementById('popup3');
+const workingBtn = document.getElementById('working');
+const closeworking = document.getElementById('button3');
+
+workingBtn.onclick = () => {
+    workingPopup.classList.add('show');
+    mainContent.classList.add('hide');
+    navHead.classList.add('hide');
+};
+
+closeworking.onclick = () => {
+    workingPopup.classList.remove('show');
+    mainContent.classList.remove('hide');
+    navHead.classList.remove('hide');
+};
+
+
+// -------- Ecommerce POPUP --------
+const commercePopup = document.getElementById('popup4');
+const commerceBtn = document.getElementById('ECommerce');
+const closecommerce = document.getElementById('button4');
+
+commerceBtn.onclick = () => {
+    commercePopup.classList.add('show');
+    mainContent.classList.add('hide');
+    navHead.classList.add('hide');
+};
+
+closecommerce.onclick = () => {
+    commercePopup.classList.remove('show');
+    mainContent.classList.remove('hide');
+    navHead.classList.remove('hide');
+};
+
+// -------- basics POPUP --------
+const basicsPopup = document.getElementById('popup5');
+const basicsBtn = document.getElementById('basics');
+const closebasics = document.getElementById('button5');
+
+basicsBtn.onclick = () => {
+    basicsPopup.classList.add('show');
+    mainContent.classList.add('hide');
+    navHead.classList.add('hide');
+};
+
+closebasics.onclick = () => {
+    basicsPopup.classList.remove('show');
+    mainContent.classList.remove('hide');
+    navHead.classList.remove('hide');
+};
+
+// -------- backend POPUP --------
+const backendPopup = document.getElementById('popup6');
+const backendBtn = document.getElementById('backend');
+const closebackend = document.getElementById('button6');
+
+backendBtn.onclick = () => {
+    backendPopup.classList.add('show');
+    mainContent.classList.add('hide');
+    navHead.classList.add('hide');
+};
+
+closebackend.onclick = () => {
+    backendPopup.classList.remove('show');
+    mainContent.classList.remove('hide');
+    navHead.classList.remove('hide');
+};
+
+
+// -------- javascript POPUP --------
+const javascriptPopup = document.getElementById('popup7');
+const javascriptBtn = document.getElementById('javascript');
+const closejavascript = document.getElementById('button7');
+
+javascriptBtn.onclick = () => {
+    javascriptPopup.classList.add('show');
+    mainContent.classList.add('hide');
+    navHead.classList.add('hide');
+};
+
+closejavascript.onclick = () => {
+    javascriptPopup.classList.remove('show');
+    mainContent.classList.remove('hide');
+    navHead.classList.remove('hide');
+};
+
+// -------- websocket POPUP --------
+const websocketPopup = document.getElementById('popup8');
+const websocketBtn = document.getElementById('websocket');
+const closewebsocket = document.getElementById('button8');
+
+websocketBtn.onclick = () => {
+    websocketPopup.classList.add('show');
+    mainContent.classList.add('hide');
+    navHead.classList.add('hide');
+};
+
+closewebsocket.onclick = () => {
+    websocketPopup.classList.remove('show');
+    mainContent.classList.remove('hide');
+    navHead.classList.remove('hide');
+};
